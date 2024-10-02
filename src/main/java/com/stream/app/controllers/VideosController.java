980ce2e48b0592bc060508d79346582298fab1a5
kanhaiya=bhayana <kanhaiyabhayana1@gmail.com>
2024-10-02 23:18:37 +0530

    Save video - added

--- src/main/java/com/stream/app/controllers/VideosController.java
+++ src/main/java/com/stream/app/controllers/VideosController.java
@@ -3,6 +3,7 @@ package com.stream.app.controllers;
 import com.stream.app.dto.ApiResponse;
 import com.stream.app.entities.Video;
 import com.stream.app.services.Interfaces.IVideoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +32,23 @@ public class VideosController {
             video.setTitle(title);
             video.setDescription(description);
             video.setVideoId(UUID.randomUUID().toString());
-            _videoService.saveVideoAsync(video,file);
-        return null;
+            Video response = _videoService.saveVideoAsync(video,file);
+        if (response != null) {
+            ApiResponse<Video> apiResponse = ApiResponse.<Video>builder()
+                    .statusCode(HttpStatus.OK.value())
+                    .message("Video uploaded successfully")
+                    .data(response)
+                    .build();
+
+            return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+        } else {
+            ApiResponse<Video> apiResponse = ApiResponse.<Video>builder()
+                    .statusCode(HttpStatus.BAD_REQUEST.value())
+                    .message("Failed to upload video")
+                    .data(null)
+                    .build();
+
+            return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/com/stream/app/services/impl/VideoServiceImpl.java
+++ src/main/java/com/stream/app/services/impl/VideoServiceImpl.java
@@ -1,23 +1,41 @@
 package com.stream.app.services.impl;
 
 import com.stream.app.entities.Video;
+import com.stream.app.repositories.IVideoRepository;
 import com.stream.app.services.Interfaces.IVideoService;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Service
 public class VideoServiceImpl implements IVideoService {
 
+    @Autowired
+    private IVideoRepository _videoRepository;
+
     Logger _logger = LoggerFactory.getLogger(VideoServiceImpl.class);
+
+    @PostConstruct
+    public void init(){
+        File file = new File(DIR);
+        if (!file.exists()){
+            file.mkdir();
+            _logger.info("Directory created with the name: "+DIR);
+        }
+    }
 
     @Value("${files.video}")
     String DIR;
@@ -28,13 +46,7 @@ public class VideoServiceImpl implements IVideoService {
 
     @Override
     public Video saveVideoAsync(Video video, MultipartFile file) {
-        // folder path : create
-        // folder path with filename
-        // copy file to the folder
-        // video metadata
-        //  save
         try{
-//            String fileName = file.getOriginalFilename();
             String contentType = file.getContentType();
 
             InputStream inputStream = file.getInputStream();
@@ -45,16 +57,17 @@ public class VideoServiceImpl implements IVideoService {
             Path path = Paths.get(dir,fileName);
             _logger.info("Path: " + path);
 
+            Files.copy(inputStream,path, StandardCopyOption.REPLACE_EXISTING);
+            video.setContentType(contentType);
+            video.setFilePath(path.toString());
 
+            return _videoRepository.save(video);
 
         }
         catch (Exception e){
             e.printStackTrace();
+            return null;
         }
-
-
-
-        return null;
     }
 
     @Override
